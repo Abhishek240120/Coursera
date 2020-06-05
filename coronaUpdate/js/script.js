084888be99675f861ded1fd757c789234e41a2d4
Abhishek240120 <58216164+Abhishek240120@users.noreply.github.com>
2020-06-05 22:41:12 +0530

    thirty five

--- coronaUpdate/js/script.js
+++ coronaUpdate/js/script.js
@@ -3,9 +3,22 @@ document.addEventListener("DOMContentLoaded",
  	    var url="https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true";
       ajaxUtils.sendGetRequest(url,
         function(res){
-
+          var i;
           var resquestAccepted=JSON.parse(res);
-          console.log(resquestAccepted['regionData'][0]['region']);
+          console.log(resquestAccepted);
+          for(i=1;i<=28;i++){
+          var w1="#tableEntry"+i.toString()+"  div:nth-child(1)";
+          var w2="#tableEntry"+i.toString()  +" div:nth-child(2)";
+          var w3="#tableEntry"+i.toString()+"  div:nth-child(3)";
+          var w4="#tableEntry"+i.toString()  +" div:nth-child(4)";
+          var w5="#tableEntry"+i.toString()+"  div:nth-child(5)";
+          document.querySelector(w1).textContent=resquestAccepted['regionData'][i-1]['region'];
+          document.querySelector(w2).textContent=resquestAccepted['regionData'][i-1]['totalInfected'];
+          document.querySelector(w3).textContent=resquestAccepted['regionData'][i-1]['recovered'];
+          document.querySelector(w4).textContent=resquestAccepted['regionData'][i-1]['deceased'];
+          document.querySelector(w5).textContent=resquestAccepted['regionData'][i-1]['totalCases'];
+      }
+
      });
 });   
 
